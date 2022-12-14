a27205d469c131d5a77758703f6eb1e02642c6aa
briabr <filalibria@yahoo.fr>
2022-12-13 21:27:52 -0500

    recreated the app, the app is functioning, debugged errors in the console

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -35,21 +35,21 @@ console.log('🚀 - data saved to the database', result);
 export const getDb = async () => {
 
 // Create a connection to the database database and version we want to use.
-  const todosDb = await openDB('todos', 1);
+  const jateDb = await openDB('jate', 1);
 
 // Create a new transaction and specify the database and data privileges.
-  const tx = todosDb.transaction('todos', 'readonly');
+  const tx = jateDb.transaction('jate', 'readonly');
 
 // Open up the desired object store.
-  const store = tx.objectStore('todos');
+  const store = tx.objectStore('jate');
 
 // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
+const request = store.get(1);
 
 // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result?.value
 }
 
 initdb();
